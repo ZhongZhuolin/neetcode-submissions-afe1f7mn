e755768e71929cb0c764b215f454577513cb6e03
Zhuolin Zhong <lin.walterzhong@gmail.com>
2026-04-01 00:02:01 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        openclose = {")" : "(", "}":"{" , "]":"["}
+        
+        for c in s:
+            if c in openclose and stack and openclose[c] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(c)
+        return not stack
+            
+
+
